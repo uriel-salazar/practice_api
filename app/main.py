39f2ca9ebc5d18da0786b7b5516a2c58034ccdce
Urieel <urielsalazar.dev@gmail.com>
2026-03-20 13:04:47 -0600

    Update .gitignore and try my get endpoint  FastAPI routes

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
-from database import Base,engine
-import uvicorn
+from fastapi.responses import HTMLResponse
 
+from database import Base,engine
+from models import User,Post
+import uvicorn
 
 app=FastAPI()
 
-
 Base.metadata.create_all(bind=engine)
 
+@app.get("/",response_class=HTMLResponse)
+async def welcome():
+    return "<h1> Welcome ! </h1>"
 
 
+
+if __name__ == "__main__":
+    uvicorn.run("main:app",
+    host="localhost", reload=True)
