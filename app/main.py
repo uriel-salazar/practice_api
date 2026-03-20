88b314d4197da0b11b24569d5ba3140c47e09a8d
Urieel <urielsalazar.dev@gmail.com>
2026-03-19 19:11:00 -0600

    Error at creating tables

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
-
+from database import Base,engine
+import uvicorn
 
 
 app=FastAPI()
+
+
+Base.metadata.create_all(bind=engine)
+
+
+
--- app/models.py
+++ app/models.py
@@ -1,4 +1,37 @@
-from database import Base
+from database import Base,engine
+from sqlalchemy.orm import Mapped,mapped_column,relationship,Session,sessionmaker
+from sqlalchemy import ForeignKey
+
+class User(Base):
+    """ A table that inherits from the declarative Base
+    it prepares a metaobject to  be mapped to a sql table.
+
+    Args:
+        Base (_type_): Declarative Base
+    """
+    __tablename__='user'
+    id:Mapped[int] = mapped_column(primary_key=True)
+    name:Mapped[str] = mapped_column(nullable=False)
+    age:Mapped[int] = mapped_column(nullable=False)
+    email:Mapped[str] = mapped_column(nullable=False,unique=True)
+    posts:Mapped[list["Post"]] =relationship("Post",back_populates='user')
+
+
+class Post(Base):
+    """ A table called "post" that it will have a relationship with 
+    user.id from the user's table. 
+
+    Args:
+        Base (_type_): _description_
+    """
+    __tablename__='post'
+    id:Mapped[int] = mapped_column(primary_key=True)
+    user_id:Mapped[int] = mapped_column(ForeignKey("user.id"))
+    name:Mapped[str] = mapped_column(nullable=False)
+    description:Mapped[str] = mapped_column(nullable=True)
+    user:Mapped['User'] = relationship('User',back_populates='posts')
+
+Session=sessionmaker(bind=engine)
 
 
 
