7331543c5e1367993a678c191dcbf4525d948b46
Urieel <urielsalazar.dev@gmail.com>
2026-03-23 13:05:53 -0600

    merge conflict resolved

--- app/main.py
+++ app/main.py
@@ -24,28 +24,6 @@ async def get_users(skip:int=1,limit: int=10,
     return crud.get_users(db,skip =skip,limit =limit)
 
     
-@app.get("/user",response_model=User_Response)
-async def get_user(user_id=int,db: Session=Depends(get_db)):
-    
-      user=db.query(User).filter(User.id == user_id).first()
-      
-      if not user:
-          HTTPException(status_code=404,detail="User not found")
-      
-      return user
-  
-@app.post("/create_users",response_model=User_Response)
-async def create_user(u_create: Create_User,db: Session = Depends(get_db)):
-
-    return crud.create_user(db,u_create)
-
-@app.get("/users/all",response_model=list[User_Response])
-async def get_users(skip:int=1,limit: int=10,
-         db:Session = Depends(get_db)):
-    
-    return crud.get_users(db,skip =skip,limit =limit)
-
-    
 @app.get("/user{id}",response_model=User_Response)
 async def get_user(user_id=int,db: Session=Depends(get_db)):
     
@@ -58,6 +36,12 @@ async def get_user(user_id=int,db: Session=Depends(get_db)):
   
 @app.post("/create_users",response_model=User_Response)
 async def create_user(u_create: Create_User,db: Session = Depends(get_db)):
+    
+    # Check if email already exists : 
+    email_exist=db.query(User).filter(User.email==User.email).first()
+    # If it exists, it raises an error.
+    if email_exist:
+        raise HTTPException(status_code=400,detail='Email already exists')
 
     return crud.create_user(db,u_create)
 
