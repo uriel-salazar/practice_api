625ebeed7cf6401cb5f97b5f7b192a1ff735eb36
Urieel <urielsalazar.dev@gmail.com>
2026-03-24 11:33:01 -0600

    Update docs and validation

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
-
 from fastapi import FastAPI,Depends,HTTPException
-from fastapi.responses import HTMLResponse,PlainTextResponse
-from .schemas import User_Response,Create_User,Update_User
+from fastapi import FastAPI,Depends,HTTPException
+from fastapi.responses import HTMLResponse
+from schemas import User_Response,Create_User
 from sqlalchemy.orm import Session
-from .database import Base,engine,get_db
-from .models import User,Post
-from . import crud
+from database import Base,engine,get_db
+from schemas import User_Response,Create_User
+from sqlalchemy.orm import Session
+from database import Base,engine,get_db
+from models import User,Post
+import crud
+import crud
 import uvicorn
 
 app=FastAPI()
@@ -24,47 +28,43 @@ async def get_users(skip:int=1,limit: int=10,
     return crud.get_users(db,skip =skip,limit =limit)
 
     
-@app.get("/user{id}",response_model=User_Response)
+@app.get("/user",response_model=User_Response)
 async def get_user(user_id=int,db: Session=Depends(get_db)):
     
       user=db.query(User).filter(User.id == user_id).first()
       
       if not user:
-           raise HTTPException(status_code=404,detail="User not found")
+          HTTPException(status_code=404,detail="User not found")
+      
+      return user
+  
+@app.post("/create_users",response_model=User_Response)
+async def create_user(u_create: Create_User,db: Session = Depends(get_db)):
+
+    return crud.create_user(db,u_create)
+
+@app.get("/users/all",response_model=list[User_Response])
+async def get_users(skip:int=1,limit: int=10,
+         db:Session = Depends(get_db)):
+    
+    return crud.get_users(db,skip =skip,limit =limit)
+
+    
+@app.get("/user",response_model=User_Response)
+async def get_user(user_id=int,db: Session=Depends(get_db)):
+    
+      user=db.query(User).filter(User.id == user_id).first()
+      
+      if not user:
+          HTTPException(status_code=404,detail="User not found")
       
       return user
   
 @app.post("/create_users",response_model=User_Response)
 async def create_user(u_create: Create_User,db: Session = Depends(get_db)):
     
-    # Check if email already exists : 
-    email_exist=db.query(User).filter(User.email==u_create.email).first()
-    # If it exists, it raises an error.
-    if email_exist:
-        raise HTTPException(status_code=400,detail='Email already exists')
-
     return crud.create_user(db,u_create)
 
-
-@app.put("update/users/{id}",response_model=User_Response)
-async def update_user(update_u:Update_User,id,db:Session=Depends(get_db)):
-    
-    user=crud.update_user(db,update_u,id)
-    
-    if user is None:
-        raise HTTPException(status_code=404,detail="User not found")
-    
-    return user
-    
-@app.delete("/delete/users/{id}")
-def delete_user(id:int,db: Session = Depends(get_db)):
-    delete=crud.delete_user(db,id)
-    
-    if delete is None:
-        raise HTTPException(status_code=400,detail="User not found")
-    
-    return f"User deleted succesful!"
-     
-  
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run("main:app",
+    host="localhost", reload=True)
