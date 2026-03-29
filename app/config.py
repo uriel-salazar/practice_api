3077bf4fba2b65911a908633f362fa2e3959def8
Urieel <urielsalazar.dev@gmail.com>
2026-03-29 16:22:19 -0600

    Create config.py to get  secret key from .env

---
+++ app/config.py
@@ -0,0 +1,21 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import secrets
+
+print(secrets.token_hex(32))
+class Settings(BaseSettings):
+    """ This class inherited from
+    Base Settings load values from .env file
+
+    """
+    model_config=SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8-",
+    )
+    secret_key:SecretStr
+    algorithm:str ="HS256"
+    # Converts env variable from string to int. 
+    access_token_expires_minutes:int = 30
+    
+    settings=Settings() #type:ignore
+    
